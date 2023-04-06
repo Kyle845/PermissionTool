847a52b83fc863d1cd35bbf27a27affd30796082
Kyle845 <738779095@qq.com>
2023-04-06 17:42:53 +0800

    完成permissiontool的开发与测试

--- app/src/main/java/com/example/permissiontool/MainActivity.kt
+++ app/src/main/java/com/example/permissiontool/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.permissiontool
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mylibrary.PermissionTool
+import com.example.permissiontool.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.makeCallBtn.setOnClickListener {
+            PermissionTool.request(this,Manifest.permission.CALL_PHONE) {allGranted,deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this,"You denied $deniedList",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 }
